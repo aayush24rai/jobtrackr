db5a124153f026b1e77ab56394d186d3961bee4c
aayushrai <aayush24rai@gmail.com>
2026-04-27 16:58:54 -0500

    feat: add all four models with relationships

--- backend/models.py
+++ backend/models.py
@@ -25,16 +25,13 @@ class Job(Base):
     __tablename__ = "jobs"
 
     id = Column(Integer, primary_key=True, index=True)
-
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-
     company = Column(String, nullable=False)
     role = Column(String, nullable=False)
-
     #this is the value in our kanban column
     status = Column(String, default="Wishlist", nullable=False)
-
     date_applied = Column(Date, nullable=True)
+    deadline = Column(Date, nullable=True)
     notes = Column(Text, nullable=True)
     url = Column(String, nullable=True)
 
@@ -42,3 +39,37 @@ class Job(Base):
     salary_max = Column(Integer, nullable=True)
 
     owner = relationship("User", back_populates="jobs")
+
+    contacts = relationship("Contact", back_populates="job", cascade="all, delete-orphan")
+
+    interviews = relationship("Interview", back_populates="job", cascade="all, delete-orphan")
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    job_id = Column(Integer, ForeignKey("jobs.id"), nullable=False)
+    name = Column(String, nullable=False)
+    role = Column(String, nullable=True)
+    outreach_method = Column(String, nullable=False)
+    outreach_date = Column(Date, nullable=True)
+    response_status = Column(String, default="no_response" , nullable=True)
+    follow_up_date = Column(Date, nullable=True)
+    notes = Column(Text, nullable=True)
+
+    job = relationship("Job", back_populates="contacts")
+
+
+
+class Interview(Base):
+    __tablename__ = "interviews"
+
+    id = Column(Integer, primary_key=True, index=True)
+    job_id = Column(Integer, ForeignKey("jobs.id"), nullable=False)
+    round = Column(String, nullable=False)
+    scheduled_date = Column(Date, nullable=True)
+    notes = Column(Text, nullable=True)
+    outcome = Column(String, default="pending", nullable=True)
+
+    job = relationship("Job", back_populates="interviews")
